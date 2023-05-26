139453c49f989f6e7f8e27b7ec3060f0e68fa87d
Jams Jams <yarinyroblero@Jamyss-iMac.local>
2023-05-26 15:40:29 -0400

    working on style

--- Script/mountain-rename.js
+++ Script/mountain-rename.js
@@ -3,6 +3,7 @@
 const mountainlocationDataDopdown = document.getElementById("mountainlocationDataDopdown");
 const mountainRow = document.getElementById("mountainRow");
 
+
 window.onload = () => {
   showMountainNameFirst();
   mountainlocationDataDopdown.onchange = DisplayMountainInfo;
@@ -18,19 +19,19 @@ function showMountainNameFirst() {
 
 function DisplayMountainInfo() {
   let selectedMountain = mountainlocationDataDopdown.value;
+  const mountainChoice = mountainsArray.find(mountain => mountain.name === selectedMountain); 
+  console.log(mountainChoice);
   mountainRow.innerHTML = ""; // Clear existing content
 
-  if (selectedMountain.length > 0) {
-    for (let mountain of mountainsArray) {
-      createMountainCard(mountain);
-    }
+  if (mountainChoice) {
+    createMountainCard(mountainChoice);
   }
 }
 
 // this is where the output will be at
 function createMountainCard(mountain) {
   let divCol = document.createElement("div");
-  divCol.className = "col-4 mx-2 my-3 row";
+  divCol.className = "col mx-2 my-3 row";
   mountainRow.appendChild(divCol);
 
   let divCard = document.createElement("div");
@@ -48,13 +49,29 @@ function createMountainCard(mountain) {
 
   let unOrderedList = document.createElement("ul");
   unOrderedList.className = "card-list";
-  h5Name.appendChild(unOrderedList);
+  divCardBody.appendChild(unOrderedList);
+
+  let mounImg = document.createElement("img");
+  mounImg.className = "mounImg";
+  mounImg.src = "images/" + mountain.img;
+  unOrderedList.appendChild(mounImg);
 
   let mounEle = document.createElement("li");
   mounEle.className = "mounEle";
-  mounEle.innerHTML = mountain.elevation;
+  mounEle.innerHTML = "Elevation: " + mountain.elevation;
   unOrderedList.appendChild(mounEle);
+
+  let mounEffort = document.createElement("li");
+  mounEffort.className = "mounEffort";
+  mounEffort.innerHTML = "Effort: " + mountain.effort;
+  unOrderedList.appendChild(mounEffort);
+
+  let mounDescription = document.createElement("li");
+  mounDescription.className = "mounDescription";
+  mounDescription.innerHTML = "Description: " + mountain.desc;
+  unOrderedList.appendChild(mounDescription);
 };
+
 
 
 /* //name: "Mt. Washington",
